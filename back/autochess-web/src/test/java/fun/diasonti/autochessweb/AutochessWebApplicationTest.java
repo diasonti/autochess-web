13c7691dc9fe83556dff805d23a5e7bb5e62967d
Vladimir Danilov <diasonti@gmail.com>
2020-01-21 01:52:59 +0600

    Add web server module

---
+++ back/autochess-web/src/test/java/fun/diasonti/autochessweb/AutochessWebApplicationTest.java
@@ -0,0 +1,13 @@
+package fun.diasonti.autochessweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class AutochessWebApplicationTest {
+
+    @Test
+    public void testContextLoads() {
+    }
+
+}
