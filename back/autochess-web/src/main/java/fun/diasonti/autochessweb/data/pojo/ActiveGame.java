a3bf111a301c55f3bb1d84184e47499dd7e7d9fd
Vladimir Danilov <diasonti@gmail.com>
2020-05-01 23:49:42 +0600

    [Back] Add match result saving.

--- back/autochess-web/src/main/java/fun/diasonti/autochessweb/data/pojo/ActiveGame.java
+++ back/autochess-web/src/main/java/fun/diasonti/autochessweb/data/pojo/ActiveGame.java
@@ -5,6 +5,7 @@ import fun.diasonti.autochessweb.data.form.UserAccountForm;
 import fun.diasonti.chessengine.data.ChessBoard;
 import fun.diasonti.chessengine.util.BoardUtils;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
@@ -21,6 +22,9 @@ public class ActiveGame {
     private List<MovablePiece> blackPieces;
 
     private ChessBoard board;
+
+    private LocalDateTime startedAt;
+    private LocalDateTime finishedAt;
 
     public ActiveGame() {
         this.id = UUID.randomUUID().toString();
@@ -88,6 +92,22 @@ public class ActiveGame {
         for (int i = 0; i < chars.length; i++) {
             blackPieces.add(new MovablePiece(i + 5, chars[i]));
         }
+    }
+
+    public LocalDateTime getStartedAt() {
+        return startedAt;
+    }
+
+    public void setStartedAt(LocalDateTime startedAt) {
+        this.startedAt = startedAt;
+    }
+
+    public LocalDateTime getFinishedAt() {
+        return finishedAt;
+    }
+
+    public void setFinishedAt(LocalDateTime finishedAt) {
+        this.finishedAt = finishedAt;
     }
 
     public void setupStartingBoard() {
